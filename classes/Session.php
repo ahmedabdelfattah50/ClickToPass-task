759b04a30ea7dd497df7b0f32b8ed5fcbb6cacd1
Ahmed Abdel-Fattah Abdel-Moaty <47077630+ahmedabdelfattah50@users.noreply.github.com>
2020-08-24 14:44:02 +0200

    Update Session.php

--- classes/Session.php
+++ classes/Session.php
@@ -5,7 +5,6 @@ class Session {
     public function session_start(){
         // the start of the session for users of the website
         session_start();
-        echo "Yes";
     }
 
     // this is function is used to set the value of any any of session when after session is started in the website
